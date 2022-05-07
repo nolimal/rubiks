ba0451f90c489d5f011d584aed67efe14ddde064
nolimal <matevz.nolimal@live.com>
2022-05-07 18:08:43 +0200

    Adding an example how to solve it

---
+++ solving_methods/examples/solve_simple_cube.py
@@ -0,0 +1,17 @@
+from game.cube import Cube
+from solving_methods.solver import Solver
+
+DEBUG = True
+
+c = Cube("DLURRDFFUBBLDDRBRBLDLRBFRUULFBDDUFBRBBRFUDFLUDLUULFLFR")
+print("Solving cube:\n", c)
+orig = Cube(c)
+solver = Solver(c)
+solver.solve()
+assert solver.cube.is_solved()
+
+print(f"{len(solver.moves)} moves: {' '.join(solver.moves)}")
+
+check = Cube(orig)
+check.sequence(" ".join(solver.moves))
+assert check.is_solved()
