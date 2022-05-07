0faeb5fb7c97509762d517ad8cedc185a8b4b13e
nolimal <matevz.nolimal@live.com>
2022-05-07 10:45:35 +0200

    - Adding helper function to obtain cc and cw rotations from face

---
+++ solving_methods/tests/test_utilities.py
@@ -0,0 +1,30 @@
+import pytest
+from solving_methods.utilities import get_rotations_from_face
+from constants import RIGHT, LEFT, UP, DOWN, FRONT, BACK
+from game.point import Point
+
+
+@pytest.mark.parametrize(
+    "face, expected_rotation_cw, expected_rotation_cc",
+    [
+        (RIGHT, "R", "Ri"),
+        (LEFT, "L", "Li"),
+        (UP, "U", "Ui"),
+        (DOWN, "D", "Di"),
+        (FRONT, "F", "Fi"),
+        (BACK, "B", "Bi")
+    ]
+)
+def test_get_rotations_from_face(face, expected_rotation_cw, expected_rotation_cc):
+    rotation_cw, rotation_cc = get_rotations_from_face(face)
+    assert expected_rotation_cw == rotation_cw
+    assert expected_rotation_cc == rotation_cc
+
+
+@pytest.fixture
+def non_face():
+    return Point(1, 1, 1)
+
+
+def test_get_rotations_from_face(non_face):
+    assert get_rotations_from_face(non_face) is None
---
+++ solving_methods/utilities.py
@@ -0,0 +1,21 @@
+from constants import RIGHT, LEFT, UP, DOWN, FRONT, BACK
+
+
+def get_rotations_from_face(face):
+    """
+    :param face: One of FRONT, BACK, LEFT, RIGHT, UP, DOWN
+    :return: A pair (CW, CC) given the clockwise and counterclockwise rotations for that face
+    """
+    if face == RIGHT:
+        return "R", "Ri"
+    elif face == LEFT:
+        return "L", "Li"
+    elif face == UP:
+        return "U", "Ui"
+    elif face == DOWN:
+        return "D", "Di"
+    elif face == FRONT:
+        return "F", "Fi"
+    elif face == BACK:
+        return "B", "Bi"
+    return None
